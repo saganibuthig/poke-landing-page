0c3b4f916114caa2b2d31b4486bd0c6c5d468739
saganibuthig <isagani.cortavista@gmail.com>
2026-04-15 21:21:17 +0800

    Add base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/poke-landing-page/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
